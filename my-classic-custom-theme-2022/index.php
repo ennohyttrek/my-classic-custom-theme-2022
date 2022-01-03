3b30e7aa67851ef219af5707aa28d35d7c487cd1
Enno Hyttrek <ehy.global@gmail.com>
2022-01-03 13:01:23 +0100

    Update

--- my-classic-custom-theme-2022/index.php
+++ my-classic-custom-theme-2022/index.php
@@ -1,19 +1,23 @@
 <?php get_header(); ?>
-    
 
-    <h1><?php bloginfo('title'); ?></h1>
 
-            <!--Loop starts-->
-            <?php 
+<h1><?php bloginfo('title'); ?></h1>
+
+<!--Loop starts-->
+<?php 
         if ( have_posts() ) : 
             while ( have_posts() ) : the_post(); 
+?>
 
-                the_content();
+<h2><?php the_title(); ?></h2>
+
+<?php
+the_content();
             
             endwhile; 
         endif; 
         ?>
-            <!--Loop ends-->
-    
-    
+<!--Loop ends-->
+
+
 <?php get_footer(); ?>
