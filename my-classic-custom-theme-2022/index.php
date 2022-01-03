67af7032b95a0726eecf18745224e2fb1a968ac4
Enno Hyttrek <ehy.global@gmail.com>
2022-01-03 12:51:28 +0100

    Update index.php

--- my-classic-custom-theme-2022/index.php
+++ my-classic-custom-theme-2022/index.php
@@ -2,6 +2,18 @@
     
 
     <h1><?php bloginfo('title'); ?></h1>
+
+            <!--Loop starts-->
+            <?php 
+        if ( have_posts() ) : 
+            while ( have_posts() ) : the_post(); 
+
+                the_content();
+            
+            endwhile; 
+        endif; 
+        ?>
+            <!--Loop ends-->
     
     
 <?php get_footer(); ?>
