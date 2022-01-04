de570509b228aa2dd24f04d05af320d83a5e6fed
Enno Hyttrek <ehy.global@gmail.com>
2022-01-04 08:44:22 +0100

    Update

--- my-classic-custom-theme-2022/functions.php
+++ my-classic-custom-theme-2022/functions.php
@@ -4,3 +4,15 @@ function custom_stylesheets() {
     wp_enqueue_style( 'style', get_stylesheet_uri()); 
 }
 add_action( 'wp_enqueue_scripts', 'custom_stylesheets' );
+
+
+function register_my_menus() {
+  register_nav_menus(
+    array(
+        'header-menu' => __( 'Main Menu' ),
+        'mobile-menu' => __( 'Mobile Menu' ),
+        'footer-menu' => __( 'Footer Menu' )
+     )
+   );
+ }
+ add_action( 'init', 'register_my_menus' );
