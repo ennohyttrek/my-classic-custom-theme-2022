c036baf741b56838f85ea1626ea1991a61e03b32
Enno Hyttrek <ehy.global@gmail.com>
2022-01-04 12:23:22 +0100

    Update

--- my-classic-custom-theme-2022/functions.php
+++ my-classic-custom-theme-2022/functions.php
@@ -2,6 +2,10 @@
 
 function custom_stylesheets() {
     wp_enqueue_style( 'style', get_stylesheet_uri()); 
+    wp_enqueue_style( 'inter-font', get_template_directory_uri() . '/fonts/inter/font-style.css' );
+    wp_enqueue_style( 'roboto-font', get_template_directory_uri() . '/fonts/roboto/font-style.css' );
+    wp_enqueue_style( 'petrona-font', get_template_directory_uri() . '/fonts/petrona/font-style.css' );
+    wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/fonts/fontawesome/css/all.css' );
 }
 add_action( 'wp_enqueue_scripts', 'custom_stylesheets' );
 
