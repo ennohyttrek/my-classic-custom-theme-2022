1bf0ee11999ed1d89a313e5ba797abb791cfae47
Enno Hyttrek <ehy.global@gmail.com>
2022-01-04 10:51:02 +0100

    Update

--- my-classic-custom-theme-2022/footer.php
+++ my-classic-custom-theme-2022/footer.php
@@ -1,6 +1,7 @@
     <nav id="footer-nav" class="footer-nav">
         <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
     </nav>
+<p>&copy; 2022 Enno Hyttrek</p>
 </footer>
 
 <?php wp_footer(); ?>
