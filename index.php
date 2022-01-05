bf33f2940e7f1ffeec5bd8b24e248428f3efa764
Enno Hyttrek <ehy.global@gmail.com>
2022-01-05 14:26:43 +0100

    Version 1.6

--- index.php
+++ index.php
@@ -1,2 +1,53 @@
-<?php
-// Silence is golden.
+<?php get_header(); ?>
+
+<header id="main-header" class="main-header">
+    <div class="inner-wrapper">
+        <h1><?php bloginfo('title'); ?></h1>
+    </div>
+</header>
+
+<nav id="main-nav" class="main-nav">
+    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+</nav>
+
+<main id="main-content" class="main-content">
+    <!--Loop starts-->
+    <?php 
+        if ( have_posts() ) : 
+            while ( have_posts() ) : the_post(); 
+?>
+
+    <article class="my-post-content">
+        <h2>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+            </a>
+        </h2>
+
+        <?php if ( has_post_thumbnail()) : ?>
+        <figure class="my-post-thumbnail">
+            <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail( 'medium' ); ?>
+            </a>
+        </figure>
+        <?php endif; ?>
+
+        <?php the_excerpt(); ?>
+    </article>
+
+
+    <?php 
+
+
+   
+            endwhile; 
+        endif; 
+?>
+    <!--Loop ends-->
+</main>
+
+<footer id="main-footer" class="main-footer">
+    <div class="inner-wrapper">
+        <p>index.php</p>
+
+        <?php get_footer(); ?>
