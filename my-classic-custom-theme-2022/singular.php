d5c934a235f29d1fdf4d3c664dfa5758a90cacc7
Enno Hyttrek <ehy.global@gmail.com>
2022-01-05 12:25:49 +0100

    Update

--- my-classic-custom-theme-2022/singular.php
+++ my-classic-custom-theme-2022/singular.php
@@ -31,7 +31,9 @@
 
         <h1 class="my-post-title"><?php the_title(); ?></h1>
         
-       
+        <div class="my-intro">
+       <?php the_excerpt(); ?>
+            </div>
 
         <?php  the_content(); ?>
     </article>
