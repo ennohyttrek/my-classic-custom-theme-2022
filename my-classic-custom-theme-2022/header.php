56c23b5cde82b7319d0c8ce928fbd81cc4bf1657
Enno Hyttrek <ehy.global@gmail.com>
2022-01-05 09:56:01 +0100

    Update

--- my-classic-custom-theme-2022/header.php
+++ my-classic-custom-theme-2022/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">s
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo('title'); ?></title>
     <?php wp_head(); ?>
 </head>
